9bb377da60b20195e6cee351bbe0998a1d77c6d6
Khawar Sultan <khawarsultan.developer@gmail.com>
2023-07-04 15:58:11 +0500

    completed 2

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import { useSelector, useDispatch } from 'react-redux'
 import { getApiConfiguration, getGenres } from './store/homeslice';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-import Header from './components/header/Header.jsx'
+import Header from './components/header/header.jsx'
 import Footer from './components/footer/Footer'
 import Home from './pages/home/Home';
 import Details from './pages/details/Details'
