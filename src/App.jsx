1f21a6382b65a251c0e540567b91d6301f318dac
Khawar Sultan <khawarsultan.developer@gmail.com>
2023-07-04 16:58:37 +0500

    fff

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Header from './components/header/header.jsx'
 import Footer from './components/footer/footer'
 import Home from './pages/home/home';
-import Details from './pages/details/details'
+import Details from './pages/details/Details'
 import SearchResult from './pages/searchResult/searchResult'
 import Explore from './pages/explore/explore.jsx'
 import PageNotFound from './pages/404/pageNotFound'
