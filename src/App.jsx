e390af8aa97dbb1aaf8e8006c6b2300a9fa9e1c0
Khawar Sultan <khawarsultan.developer@gmail.com>
2023-07-04 16:00:14 +0500

    hi

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import { getApiConfiguration, getGenres } from './store/homeslice';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import Header from './components/header/header.jsx'
-import Footer from './components/footer/Footer'
+import Footer from './components/footer/footer'
 import Home from './pages/home/Home';
 import Details from './pages/details/Details'
 import SearchResult from './pages/searchResult/searchResult'
