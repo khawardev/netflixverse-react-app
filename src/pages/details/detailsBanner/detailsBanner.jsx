1dbc00731eeea03a82ea4a6d02af884f89880d3b
Khawar Sultan <khawarsultan.developer@gmail.com>
2023-07-04 16:39:02 +0500

    genred

--- src/pages/details/detailsBanner/detailsBanner.jsx
+++ src/pages/details/detailsBanner/detailsBanner.jsx
@@ -7,7 +7,7 @@ import { useParams, useLocation } from "react-router-dom";
 import { useSelector } from "react-redux";
 import dayjs from "dayjs";
 import useFetch from "../../../hooks/Usefetch";
-import Genres from "../../../components/genres/Genres";
+import Genres from "../../../components/genres/Genres.jsx";
 import CircleRating from "../../../components/circleRating/circleRating";
 import Img from "../../../components/lazyLoadImage/LazyloadImage.jsx";
 import PosterFallback from "../../../assets/no-poster.png";
