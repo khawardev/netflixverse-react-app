88bf9c6116bcf94608fd2449d5bebe309711025a
Khawar Sultan <khawarsultan.developer@gmail.com>
2023-07-05 13:35:35 +0500

    asas

--- src/pages/details/detailsBanner/detailsBanner.jsx
+++ src/pages/details/detailsBanner/detailsBanner.jsx
@@ -4,11 +4,10 @@
 /* eslint-disable no-unused-vars */
 import './detailsBanner.scss';
 import React, { useState, useEffect } from "react";
-import { useParams, useLocation } from "react-router-dom";
+import { useParams} from "react-router-dom";
 import { useSelector } from "react-redux";
-import dayjs from "dayjs";
 import useFetch from "../../../hooks/Usefetch";
-import genres from "../../../components/genres/Genres";
+import GenresComp from "../../../components/genres/Genres";
 import CircleRating from "../../../components/circleRating/circleRating";
 import Img from "../../../components/lazyLoadImage/LazyloadImage.jsx";
 import PosterFallback from "../../../assets/no-poster.png";
@@ -132,7 +131,7 @@ const DetailsBanner = ({ video, crew }) => {
                                         <div className='d-flex gap-3 align-items-center py-3'>
                                             <CircleRating width={'55px'} height={'55px'} Rating={data.vote_average.toFixed(1)} />
 
-                                            <genres fontSize="16px" data={data.genres.map(genre => genre.id).slice(0, 2)} />
+                                            <GenresComp fontSize="16px" data={data.genres.map(genre => genre.id).slice(0, 2)} />
 
                                         </div>
 
